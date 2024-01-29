47e2a1fdaba6592aa2700e39cf4d77aef3f688c8
Kornel Światłowski <146420031+kSwiatlowski-hacken@users.noreply.github.com>
2024-01-29 11:07:57 +0100

    Update testTemplate.js

--- testTemplate.js
+++ testTemplate.js
@@ -1,4 +1,5 @@
 const { time, loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+//const { time, loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
